ba294b626ce9d0dfa698e37fa9e3d56caa44e625
Sahaj Singh <70798888+Bigguysahaj@users.noreply.github.com>
2022-06-16 02:48:20 +0530

    Create BURGERS.cpp

---
+++ codechef/Starters 43/BURGERS.cpp
@@ -0,0 +1,25 @@
+https://www.codechef.com/START43D/problems/BURGERS/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(0);
+#define ll long long
+
+
+int main() {
+    fastio;
+    int test;
+    cin >> test;
+    while(test--)
+    {
+        
+	int a,b;
+	cin>>a>>b;
+	
+	cout<<std::min(a,b)<<endl;
+	
+    }
+	return 0;
+}
