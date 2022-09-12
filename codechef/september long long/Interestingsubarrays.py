17fe8f999f246673a9c52435730bcfd39bc56a98
Sahaj Singh <70798888+Bigguysahaj@users.noreply.github.com>
2022-09-12 13:50:59 +0530

    added interesting subarrays

---
+++ codechef/september long long/Interestingsubarrays.py
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(0);
+#define ll long long
+
+
+int main() {
+    fastio;
+    int test;
+    cin >> test;
+    while(test--)
+    {
+        
+	// your code goes here
+	
+    }
+	return 0;
+}
